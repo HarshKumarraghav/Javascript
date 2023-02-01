2a7545378bceb9e4b691c4a341aa56ae4ffc3730
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-02-02 00:35:24 +0530

    BigO Notation: example added

---
+++ BigONotation/1.AddUpto(examples)/2_example_1.js
@@ -0,0 +1,8 @@
+function AddUpTo1(n) {
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+AddUpTo1(10);
---
+++ BigONotation/1.AddUpto(examples)/3_example_2.js
@@ -0,0 +1,6 @@
+function AddUpTo2(n) {
+  let total;
+  total = (n * (n + 1)) / 2;
+  return total;
+}
+AddUpTo2(10);
