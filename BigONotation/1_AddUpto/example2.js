5efd8fc13ac5f9d35c42fc93b09ee7a9ee19d59c
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-02-03 02:09:11 +0530

    added important comments

--- BigONotation/1_AddUpto/example2.js
+++ BigONotation/1_AddUpto/example2.js
@@ -1,3 +1,8 @@
+/**
+ * AddUpTo2 takes in a number n and returns the sum of all numbers from 1 to n.
+ * @param n - the number we want to add up to
+ * @returns the total of the sum of all the numbers from 1 to n.
+ */
 function AddUpTo2(n) {
   let total;
   total = (n * (n + 1)) / 2;
--- BigONotation/2_LogAtLeast5/example.js
+++ BigONotation/2_LogAtLeast5/example.js
@@ -1,3 +1,8 @@
+/**
+ * LogAtLeast5 logs at least 5 numbers, starting with 1, but will log more if a larger number is passed
+ * in.
+ * @param n - the number of times to log
+ */
 function LogAtLeast5(n) {
   for (let i = 1; i <= Math.max(5, n); i++) {
     console.log(i);
