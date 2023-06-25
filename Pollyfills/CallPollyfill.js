83f3c1a1815989b6451660607ae6b90706df33c4
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-06-25 18:04:47 +0530

    pollyfill for call, apply and bind done

--- Pollyfills/CallPollyfill.js
+++ Pollyfills/CallPollyfill.js
@@ -1,6 +1,6 @@
 // Firstly how Call work
-function getFullName() {
-  return `${this.firstName} ${this.lastName}`;
+function getFullName(age, dessignation) {
+  return `${this.firstName} ${this.lastName} is ${age} and he is a ${dessignation}`;
 }
 const User = {
   firstName: "John",
@@ -14,4 +14,24 @@ const User2 = {
   age: 20,
 };
 
-User.getFullName.call(User2);
+console.log(User.getFullName.call(User2, User2.age, "Software Engineer"));
+
+// let's write own version of call method
+Function.prototype.myCall = function (context, ...args) {
+  if (typeof context !== "object" || context === null) {
+    throw new TypeError("Invalid context. Context must be an object.");
+  }
+  if (typeof this !== "function") {
+    throw new TypeError("This is not the function");
+  }
+  const uniqueid = `${Date.now()}`;
+  context[uniqueid] = this;
+  const result = context[uniqueid](...args);
+  delete context[uniqueid];
+  return result;
+};
+
+console.log(
+  "My Call",
+  User.getFullName.myCall(User2, User2.age, "Software Engineer")
+);
