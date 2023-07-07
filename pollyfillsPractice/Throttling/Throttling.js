419942c6f98378334bc82d6a51da4d8cc3b3064a
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-07-07 19:46:24 +0530

    throttle code practice

--- pollyfillsPractice/Throttling/Throttling.js
+++ pollyfillsPractice/Throttling/Throttling.js
@@ -1,4 +1,4 @@
-const myThrottle = (fn, t) => {
+const myThrottleCode = (fn, t) => {
   let last = 0;
   const throttle = (...args) => {
     let now = new Date().getTime();
@@ -12,5 +12,5 @@ const clickBtn = document.getElementsByClassName("btn_click");
 const clickHanlder = () => {
   console.log("click");
 };
-const betterClick = myThrottle(clickHanlder, 2000);
+const betterClick = myThrottleCode(clickHanlder, 2000);
 clickBtn[0].addEventListener("click", betterClick);
