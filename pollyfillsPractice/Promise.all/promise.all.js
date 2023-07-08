9bbc18ee2ae6f44d8cfda7abc8663c716e545cf1
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-07-08 20:06:56 +0530

    promise.all practice done

--- pollyfillsPractice/Promise.all/promise.all.js
+++ pollyfillsPractice/Promise.all/promise.all.js
@@ -29,25 +29,27 @@ Promise.all([Dummy1(5000), Dummy2(2000), Dummy3(4000)]).then((res) =>
   console.log(res)
 );
 
-const myPromiseAllCode = (args) => {
+const myPromiseAll = (args) => {
   if (!Array.isArray(args)) throw new TypeError("not a valid array");
   const pr = new Promise((res, rej) => {
     const output = [];
     let count = 0;
     args.forEach((promise, index) => {
       promise
-        .then((ressult) => {
-          output[index] = ressult;
+        .then((result) => {
+          output[index] = result;
           count++;
           if (count === args.length) {
             res(output);
           }
         })
-        .catch((err) => rej(err));
+        .catch((err) => {
+          rej(err);
+        });
     });
   });
   return pr;
 };
-myPromiseAllCode([Dummy1(5000), Dummy2(2000), Dummy3(4000)]).then((res) =>
+myPromiseAll([Dummy1(5000), Dummy2(2000), Dummy3(4000)]).then((res) =>
   console.log(res)
 );
