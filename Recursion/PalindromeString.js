248f6c332b745bdb249f0c6448128822b5cb248c
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-07-11 23:13:14 +0530

    Paligdrome done

--- Recursion/PalindromeString.js
+++ Recursion/PalindromeString.js
@@ -1,20 +1,7 @@
-/**
- * This is a recursive function that checks if a given string is a palindrome.
- * @param s - The input string that we want to check if it is a palindrome or not.
- * @param i - The parameter "i" in the function "palindrome" represents the index of the character
- * being compared in the string "s". It starts at 0 and increments by 1 recursively until it reaches
- * the middle of the string.
- * @returns a boolean value - true if the input string is a palindrome (reads the same forwards and
- * backwards), and false otherwise.
- */
 function palindrome(s, i) {
-  let length = s.length;
-  if (i >= length / 2) {
-    return true;
-  }
-  if (s[i] != s[length - i - 1]) {
-    return false;
-  }
+  const n = s.length;
+  if (i >= n / 2) return true;
+  if (s[i] !== s[n - i - 1]) return false;
   return palindrome(s, i + 1);
 }
 let s = "madam";
