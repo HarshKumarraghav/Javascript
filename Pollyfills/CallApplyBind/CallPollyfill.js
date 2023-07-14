029fa50bbb47bf8cc788b8777ba6298e10881eb7
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-07-14 20:21:32 +0530

    Call practice dobe

--- Pollyfills/CallApplyBind/CallPollyfill.js
+++ Pollyfills/CallApplyBind/CallPollyfill.js
@@ -17,21 +17,19 @@ const User2 = {
 console.log(User.getFullName.call(User2, User2.age, "Software Engineer"));
 
 // let's write own version of call method
-Function.prototype.myCall = function (context, ...args) {
-  if (typeof context !== "object" || context === null) {
-    throw new TypeError("Invalid context. Context must be an object.");
-  }
-  if (typeof this !== "function") {
-    throw new TypeError("This is not the function");
-  }
-  const uniqueid = `${Date.now()}`;
-  context[uniqueid] = this;
-  const result = context[uniqueid](...args);
-  delete context[uniqueid];
+Function.prototype.myCallFunction = function (context, ...args) {
+  if (typeof context !== "object")
+    throw new TypeError(`${context} not a valid object`);
+  if (typeof this !== "function")
+    throw new TypeError(`${this} not a valid object`);
+  const Index = `${Date.now()}`;
+  context[Index] = this;
+  const result = context[Index](...args);
+  delete context[Index];
   return result;
 };
 
 console.log(
   "My Call",
-  User.getFullName.myCall(User2, User2.age, "Software Engineer")
+  User.getFullName.myCallFunction(User2, User2.age, "Software Engineer")
 );
