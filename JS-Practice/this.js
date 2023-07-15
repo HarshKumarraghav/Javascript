628d23910e1514d1dc7df2572cd74b80bed910f1
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-07-15 22:07:40 +0530

    Apply practice done

--- JS-Practice/this.js
+++ JS-Practice/this.js
@@ -16,6 +16,6 @@ console.log(obj1.sayMyName(20, "Software Engineer"));
 // understanding of "call" function.
 console.log(obj1.sayMyName.call(obj2, 20, "Software Engineer"));
 // understanding of "apply" function.
-console.log(obj1.sayMyName.apply(obj2, [20, "Software Engineer"]));
+console.log(obj1.sayMyName.apply(obj2, [10, "Software Engineer"]));
 // understanding of "bind" function.
 console.log(obj1.sayMyName.bind(obj2, 20, "Software Engineer"));
