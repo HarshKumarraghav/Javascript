81e1d17a22a3f1deb05c5f5c56365caf99c4ace2
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-08-01 17:30:00 +0530

    Event bubbling and capturing

---
+++ Event_Propogation/index.js
@@ -0,0 +1,9 @@
+document
+  .querySelector("#grandParent")
+  .addEventListener("click", () => console.log("GrandParent Clicked!"), true);
+document
+  .querySelector("#parent")
+  .addEventListener("click", () => console.log("Parent Clicked!"), true);
+document
+  .querySelector("#child")
+  .addEventListener("click", () => console.log("child Clicked!"), true);
