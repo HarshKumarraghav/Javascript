05e9e0b5c668bfb1fc15edbc3cbcd279be684d00
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-08-09 01:23:11 +0530

    ContainDuplicate

--- Array/ContainDuplicate.js
+++ Array/ContainDuplicate.js
@@ -4,11 +4,13 @@
  */
 
 var containsDuplicate = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i + 1]) return true;
+  const sortedNumber = nums.sort();
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    if (sortedNumber[i] === sortedNumber[i + 1]) return true;
   }
   return false;
 };
-const nums = [1, 2, 3, 1].sort();
+const nums = [1, 2, 3, 4];
 const result = containsDuplicate(nums);
 console.log(result);
