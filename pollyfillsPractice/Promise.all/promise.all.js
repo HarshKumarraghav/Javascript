d3b45ce6fdc128b1405140f4e37d8498ba9a528b
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-08-15 00:25:29 +0530

    Promise.all practice

--- pollyfillsPractice/Promise.all/promise.all.js
+++ pollyfillsPractice/Promise.all/promise.all.js
@@ -29,27 +29,23 @@ Promise.all([Dummy1(5000), Dummy2(2000), Dummy3(4000)]).then((res) =>
   console.log(res)
 );
 
-const myPromiseAllCode = (args) => {
+const myPromiseAll = (args) => {
   if (!Array.isArray(args)) {
     throw new TypeError("Not a valid argument");
   }
   const pr = new Promise((res, rej) => {
-    const output = [];
     let count = 0;
-    args.forEach((element, index) => {
-      element
-        .then((result) => {
-          output[index] = result;
-          count++;
-          if (count === args.length) {
-            res(output);
-          }
-        })
-        .catch((err) => rej(err));
+    const output = [];
+    args.forEach((Element, index) => {
+      Element.then((result) => {
+        output[index] = result;
+        count++;
+        if (count === args.length) res(output);
+      }).catch((err) => rej(err));
     });
   });
   return pr;
 };
-myPromiseAllCode([Dummy1(5000), Dummy2(2000), Dummy3(4000)]).then((res) =>
+myPromiseAll([Dummy1(5000), Dummy2(2000), Dummy3(4000)]).then((res) =>
   console.log(res)
 );
