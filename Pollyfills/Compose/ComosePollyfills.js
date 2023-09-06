bc2d7de269806e9778177e42e69cc58eed049f9f
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-09-06 21:51:33 +0530

    Compose and pipe pollyfill done

--- Pollyfills/Compose/ComosePollyfills.js
+++
@@ -1,6 +0,0 @@
-const add2 = (num) => num + 2;
-const sub1 = (num) => num - 1;
-const multipleBy5 = (num) => num * 5;
-const GivePositiveOutput = (num) => Math.abs(num);
-
-const result = GivePositiveOutput(multipleBy5(sub1(add2(6))));
--- Pollyfills/ComposePipe/Compose/ComposePollyfills.js
+++ Pollyfills/ComposePipe/Compose/ComposePollyfills.js
@@ -1,9 +1,9 @@
 // function composing is concept in functional programing to execute more than one function in give order...
 
-const add2 = (num) => num + 2;
-const sub1 = (num) => num - 30;
-const multipleBy5 = (num) => num * 5;
-const givePositiveOutput = (num) => Math.abs(num);
+const Add2 = (num) => num + 2;
+const Sub30 = (num) => num - 30;
+const MultipleBy5 = (num) => num * 5;
+const GivePositiveOutput = (num) => Math.abs(num);
 
 const result = givePositiveOutput(multipleBy5(sub1(add2(6))));
 
@@ -14,5 +14,5 @@ const compose =
   (input) =>
     fn.reduceRight((currentValue, func) => func(currentValue), input);
 
-const composeFunc = compose(givePositiveOutput, multipleBy5, sub1, add2)(6);
+const composeFunc = compose(GivePositiveOutput, MultipleBy5, Sub30, Add2)(6);
 console.log(composeFunc);
