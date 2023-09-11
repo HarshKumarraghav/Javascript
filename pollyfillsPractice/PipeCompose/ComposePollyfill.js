7a54da0561c14d5483ac502abba6d0f7e593dd9e
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-09-11 17:11:17 +0530

    compose pollyfill done

---
+++ pollyfillsPractice/PipeCompose/ComposePollyfill.js
@@ -0,0 +1,9 @@
+const Add2 = (num) => num + 2;
+const Sub30 = (num) => num - 30;
+const MultipleBy5 = (num) => num * 5;
+const GivePositiveOutput = (num) => Math.abs(num);
+
+const compose =
+  (...fns) =>
+  (input) =>
+    fns.reduceRight((currentVal, func) => func(currentVal), input);
