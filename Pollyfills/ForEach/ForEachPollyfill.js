828c303e94e469641bd456230081e461d92d0acb
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-09-16 22:13:34 +0530

    ForEach pollyfill done

---
+++ Pollyfills/ForEach/ForEachPollyfill.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5];
+arr.forEach((element) => console.log(element * 2));
+
+Array.prototype.myForEach = function (cf) {
+  if (!Array.isArray(this))
+    throw new TypeError(`${this}.myForEach is not a function`);
+  for (let i = 0; i < this.length; i++) {
+    cf(this[i]);
+  }
+};
+
+arr.myForEach((element) => console.log("myForEach", element * 2));
