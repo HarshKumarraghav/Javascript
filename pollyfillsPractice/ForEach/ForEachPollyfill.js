ccab81b544d9667a84192dc952fbef82604d510a
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-09-17 15:01:28 +0530

    error boundary more precise

--- pollyfillsPractice/ForEach/ForEachPollyfill.js
+++ pollyfillsPractice/ForEach/ForEachPollyfill.js
@@ -2,7 +2,7 @@ let arr = [1, 2, 3, 4, 5];
 arr.forEach((element) => console.log(element * 2));
 Array.prototype.myForEach = function (cf) {
   if (!Array.isArray(this))
-    throw new TypeError(`${this}.myForEach is not a function`);
+    throw new TypeError(`${this}.myForEach is not a valid function`);
 
   for (let i = 0; i < this.length; i++) {
     cf(this[i]);
