3370e679f08f2cd61c7b67f283573fcbdf5f273f
HarshKumarraghav <raghavharsh068@gmail.com>
2023-09-22 00:07:14 +0530

    FoodCompany/pollyfill: Throttle pollyfill done

---
+++ FoodCompany/Pollyfill/Throttle.js/Throttle.js
@@ -0,0 +1,18 @@
+let count = 0;
+const ButtonClickHandler = () => {
+  console.log("count", count++);
+};
+const MyThrottle = (fn, d) => {
+  if (typeof fn !== "function") throw new TypeError("Not a valid function");
+  let last = 0;
+  const throttle = (...args) => {
+    let now = new Date().getTime();
+    if (now - last < d) return;
+    last = now;
+    fn(...args);
+  };
+  return throttle;
+};
+
+const btn_Click = document.getElementsByClassName("btn_click");
+btn_Click[0].addEventListener("click", MyThrottle(ButtonClickHandler, 2000));
