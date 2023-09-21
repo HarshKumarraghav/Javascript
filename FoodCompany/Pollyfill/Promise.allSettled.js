f9b1db21dc3d5cc97d4cc9516fc7c7b755cac244
HarshKumarraghav <raghavharsh068@gmail.com>
2023-09-21 23:05:29 +0530

    FoodCompany/pollyfill: Promise.all and promise.allSettled pollyfill done

---
+++ FoodCompany/Pollyfill/Promise.allSettled.js
@@ -0,0 +1,37 @@
+const Dummy1 = (delay) => {
+  const pr = new Promise((res, rej) => {
+    setTimeout(() => {
+      res({
+        status: "fullfilled",
+        time: delay,
+      });
+    }, delay);
+  });
+  return pr;
+};
+const Dummy2 = (delay) => {
+  const pr = new Promise((res, rej) => {
+    setTimeout(() => {
+      res({
+        status: "fullfilled",
+        time: delay,
+      });
+    }, delay);
+  });
+  return pr;
+};
+const Dummy3 = (delay) => {
+  const pr = new Promise((res, rej) => {
+    setTimeout(() => {
+      rej({
+        status: "rej",
+        time: delay,
+      });
+    }, delay);
+  });
+  return pr;
+};
+
+Promise.allSettled([Dummy1(5000), Dummy2(2000), Dummy3(4000)]).then((result) =>
+  console.log(result)
+);
