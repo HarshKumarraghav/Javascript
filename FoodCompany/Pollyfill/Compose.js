81780bca737e56f333b66a45689d8d984ccb7e11
HarshKumarraghav <raghavharsh068@gmail.com>
2023-09-21 23:12:20 +0530

    FoodCompany/pollyfill: compose pollyfill done

---
+++ FoodCompany/Pollyfill/Compose.js
@@ -0,0 +1,20 @@
+const Add2 = (num) => num + 2;
+
+const Sub30 = (num) => num - 30;
+
+const Multiply10 = (num) => num * 10;
+
+const IsPositive = (num) => Math.abs(num);
+
+const result = IsPositive(Multiply10(Sub30(Add2(10))));
+
+console.log("result", result);
+
+const compose =
+  (...fns) =>
+  (input) =>
+    fns.reduceRight((currentValue, func) => func(currentValue), input);
+
+const res = compose(IsPositive, Multiply10, Sub30, Add2)(10);
+
+console.log("res", res);
