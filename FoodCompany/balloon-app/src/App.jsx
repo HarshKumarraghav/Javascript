b60d2b108ee9ef9cd2035d886f6f44f8c7e0e5a8
HarshKumarraghav <raghavharsh068@gmail.com>
2023-09-23 20:54:10 +0530

    FoodCompany/balloon-app: tictactoe practice done

--- FoodCompany/balloon-app/src/App.jsx
+++ FoodCompany/balloon-app/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Pagination from "./pages/Pagination";
 import Star from "./pages/Star";
+import TicTacToe from "./pages/TicTacToe";
 function App() {
   return (
     <Routes>
@@ -11,6 +12,7 @@ function App() {
       <Route path="/todo" element={<Todo />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/star" element={<Star />} />
+      <Route path="/tictactoe" element={<TicTacToe />} />
       <Route path="/pagination" element={<Pagination />} />
     </Routes>
   );
