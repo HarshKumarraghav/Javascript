8de48ca6eba7437117fa2a7461dca80e30cb27b6
HarshKumarraghav <raghavharsh068@gmail.com>
2023-10-19 15:41:47 +0530

    Tic Tac Toe practice done

--- JS-Practice/balloon-app/src/App.jsx
+++ JS-Practice/balloon-app/src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import TicTacToe from "./Pages/TicTacToe";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<TicTacToe />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
